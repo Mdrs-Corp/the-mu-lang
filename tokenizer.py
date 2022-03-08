82d524f71b4f71bc51e04bb565bd09fefecd7b97
Ravelfett <ravelfett@gmail.com>
2022-03-08 20:19:11 +0100

    number literals

--- tokenizer.py
+++ tokenizer.py
@@ -1,10 +1,19 @@
 ##converti le programe mu en une liste de tokens, en gros juste pour separer les "mots"
+
+letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+numbers = ",0123456789"
+
+
 def tokenize(str):
     tokens = []
     index = 0
     while index < len(str):
         chr = str[index]
-        if chr == "<":
+
+        if chr == " ":
+            index += 1
+
+        elif chr == "<":
             name = ""
             index += 1
             while index < len(str) and str[index] != ">":
@@ -12,6 +21,14 @@ def tokenize(str):
                 index += 1
             index += 1
             tokens.append(Token("balise", name))
+
+        elif chr in numbers:
+            text = ""
+            while index < len(str) and str[index] in numbers:
+                text += str[index]
+                index += 1
+            tokens.append(Token("literal", text))
+
     return tokens
 
 
@@ -24,4 +41,4 @@ class Token:
     def __repr__(self):
         return self.type + ": " +self.value
 
-print(tokenize("<oh></br></oh>"))
+print(tokenize("<oh></br> 3    5    <add>3 5</add></oh>"))
