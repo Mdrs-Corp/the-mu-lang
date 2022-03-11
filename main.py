066292efc2642089cfe532c131145d90f6f794b6
Ravelfett <ravelfett@gmail.com>
2022-03-11 14:57:51 +0100

    path changed

--- main.py
+++ main.py
@@ -2,18 +2,7 @@ import tokenizer
 import parser
 
 
-exemple="""
-<µ>
-	<loq>
-		<add>1 2</add>
-	</loq>
-	<pam>
-		<add>1 2</add>
-	</pam>
-</µ>
-"""
-fib=open("./fibonacci.µ","r")
-hw=open("./helloworld.µ","r")
+hw=open("./muPrograms/helloworld.µ","r")
 
 tokens = tokenizer.tokenize(hw.read())
 print(tokens)
