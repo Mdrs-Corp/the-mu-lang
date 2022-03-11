4520114fed7e00e82ddb06bd89c4eba4e0457d72
Ravelfett <ravelfett@gmail.com>
2022-03-11 15:19:41 +0100

    <     >

--- nodes.py
+++ nodes.py
@@ -1,35 +1,35 @@
 def get(node):
-	if type(node)==Num or type(node)==Fil:
+	if type(node) == Num or type(node) == Fil:
 		return node.value
 	else:
 		return node.action()
 
 class Node():
-	REPR="µ"
-	def __init__(self,value=None):
-		self.childs=[]
-		self.value=value
+	REPR = "µ"
+	def __init__(self, value = None):
+		self.childs = []
+		self.value = value
 
 	def __repr__(self):
-		t=f"{self.REPR}[{self.value}]("
+		t = f"{self.REPR}[{self.value}]("
 		for e in self.childs:
-			t+="\n\t"+str(e)
+			t += "\n\t" + str(e)
 
-		return t+f"){self.REPR[0]}\n"
+		return t + f"){self.REPR[0]}\n"
 	def action(self):
 		for e in self.childs:
 			e.action()
 
 class Loq(Node):
-	REPR="Loqum"
-	def __init__(self,v):
-		self.value="%"
+	REPR = "Loqum"
+	def __init__(self, v):
+		self.value = "%"
 		super().__init__()
 
 	def action(self):
-		self.value=""
+		self.value = ""
 		for elem in self.childs:
-			self.value+=str(get(elem))
+			self.value += str(get(elem))
 		print(self.value)
 		return self.value
 
@@ -38,54 +38,63 @@ class Loq(Node):
 
 
 class Add(Node):
-	REPR="Addere"
-	def __init__(self,v):
+	REPR = "Addere"
+	def __init__(self, v):
 		super().__init__(0)
+
 	def action(self):
 		for enfant in self.childs:
-			self.value+=get(enfant)
+			self.value += get(enfant)
 		return self.value
 
 class Partio(Node):
-	REPR="Partiorum"
-	def __init__(self,v):
+	REPR = "Partiorum"
+	def __init__(self, v):
 		super().__init__(1)
+
 	def action(self):
-		self.value=get(self.childs[0])
+		self.value = get(self.childs[0])
 		for enfant in self.childs[1:]:
-			self.value/=get(enfant)
+			self.value /= get(enfant)
 		return self.value
 
 class Mul(Node):
-	REPR="multiplicare"
-	def __init__(self,v):
+	REPR = "multiplicare"
+	def __init__(self, v):
 		super().__init__(1)
+		
 	def action(self):
 		for enfant in self.childs:
-			self.value*=get(enfant)
+			self.value *= get(enfant)
 		return self.value
 class Num(Node):
 	# C'est un literal je devrai faire autrement je pense
-	REPR="Numerus"
-	def __init__(self,value):
+	REPR = "Numerus"
+	def __init__(self, value):
 		super().__init__(int(value))
 
 class Fil(Node):
 	# Aussi un literal mais bon bref
-	REPR="Filum"
-	def __init__(self,value):
+	REPR = "Filum"
+	def __init__(self, value):
 		super().__init__(value)
 
 
-bigdic={"loq":Loq,".µ":Node,
-"µ":Node,"add":Add,"partio":Partio,"mul":Mul}
+bigdic={
+	"loq":Loq,
+	".µ":Node,
+	"µ":Node,
+	"add":Add,
+	"partio":Partio,
+	"mul":Mul
+}
 
 def newnode(token):
-	print(token.type,token.value)
-	if token.type=="balise":
+	print(token.type, token.value)
+	if token.type == "balise":
 		typ=bigdic[token.value]
 		return typ(token.value)
-	elif token.type=="number":
+	elif token.type == "number":
 		return Num(token.value)
-	elif token.type=="string":
+	elif token.type == "string":
 		return Fil(token.value)
