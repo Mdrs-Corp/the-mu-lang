794581787d01d04c17c0fc473835368941749be3
rayou <momoladebrouill@gmail.com>
2022-04-11 16:06:22 +0200

    clean

--- cµ/tokenizer.c
+++ cµ/tokenizer.c
@@ -1,105 +1,110 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#define size malloc(sizeof(token))
 /* les types:
 0: string
 1: balise
 2: number
 3: identifier
 */
-const char exemple[]="<µ>\n<loq> || Hello world ! || </loq>\n</µ>\n";
+
+const char exemple[]="<µ> <loq> || Hello world ! || 125 </loq>  </µ> \0";
 
 int isletter(char s){return ('a'<=s && s<='z') || ('A'<=s && s<='Z');}
 int isnumber(char s){return ('0'<=s && s<='9') || s=='.' || s=='-';}
 
 typedef struct token {
   int type;
-  char value[100];
+  char value[100]; // Les valeurs des tokens sont ainsi limités à 100 caractères
   struct token * next;
 }token ;
 
 token * tokenize(const char text[],int len){
-  token * tokens = (token*)malloc(sizeof(token));
-  tokens->next=malloc(sizeof(token));
-  token * using = tokens->next;
-  int index=0;
-  char c=text[index];
-  while(index<len&&c!='\0'){
-    char c=text[index];
-    if(c==' ' || c=='\n' || c=='\t'){ index++;}
-    else if(c=='|' && index+1<len){
-      int start=index;
-      if(text[index+1]=='|'){
-        index++;
-        while(text[index]!='|' && text[index+1]!= '|'){index++;}
-        index+=2;
-      }
-      using->next=malloc(sizeof(token));
-      using=using->next;
-      using->type=0;
-      int e=0;
-      char p[index-start-4+1];
-      for(int i=start+2;i<index-2;){p[e]=text[i];e++;}
-      p[e+1]='\0';
-      strcpy(using->value,p);
-    }else if(c=='<'){
-      char name[100];
-      index++;
-      int e=0;
-      while(index<len && text[index] !='>'){
-        name[e]=text[index];
-        e++;
-        index++;
-      }
-      index++;
-      using->next=malloc(sizeof(token));
-      using=using->next;
-      using->type=1;
-      name[e+1]='\0';
-      strcpy(using->value,name);
-    }else if(isnumber(c)){
-      char name[100];
-      index++;
-      int e=0;
-      while(index<len && isnumber(text[index])){
-        name[e]=text[index];
-        e++;
-        index++;
-      }
-      using->next=malloc(sizeof(token));
-      using=using->next;
-      using->type=2;
-      name[e+1]='\0';
-      strcpy(using->value,name);
-    }else if(isletter(c)){
-    char name[100];
-    index++;
-    int e=0;
-    while(index<len && isletter(text[index])){
-      name[e]=text[index];
-      e++;
-      index++;
-    }
-    using->next=malloc(sizeof(token));
-    using=using->next;
-    using->type=3;
-    name[e+1]='\0';
-    strcpy(using->value,name);
-  }else{
-    printf("Pas compris : %c ( place dans utf : %i)\n",c,c );
-    index++;
-  }
-
-  }
-  return tokens;
+	token * tokens = (token*) size;
+	tokens -> next = (token*) size;
+	token * using = tokens -> next;
+	int index=0;
+	char c;
+	while(index < len ){
+		c=text[index];
+		if(c==' ' || c=='\n' || c=='\t'){
+			index++;
+		}else if(c=='|' && index+1<len){
+			int lenOfStr=0;
+			if(text[index+1]=='|'){
+				index++;
+				while(text[index]!='|' && text[index+1]!= '|'){
+					index++;
+					lenOfStr++;
+				}
+			}
+			using->next=(token*)size;
+			using=using->next;
+			using->type=0;
+			char p[lenOfStr+1];
+			for(int i=0;i<lenOfStr;i++){
+				p[i]=text[index-lenOfStr+i];
+			}
+			p[lenOfStr+1]='\0';
+			strcpy(using->value,p);
+		}else if(c=='<'){
+			char name[100];
+			index++;
+			int e=0;
+			while(index<len && text[index] !='>'){
+				name[e]=text[index];
+				e++;
+				index++;
+			}
+			index++;
+			using->next=(token *) size;
+			using=using->next;
+			using->type=1;
+			name[e+1]='\0';
+			strcpy(using->value,name);
+		}else if(isnumber(c)){
+			char name[100];
+			index++;
+			int e=0;
+			while(index<len && isnumber(text[index])){
+				name[e]=text[index];
+				e++;
+				index++;
+			}
+			using->next=(token *) size;
+			using=using->next;
+			using->type=2;
+			name[e+1]='\0';
+			strcpy(using->value,name);
+		}else if(isletter(c)){
+			char name[100];
+			index++;
+			int e=0;
+			while(index<len && isletter(text[index])){
+				name[e]=text[index];
+				e++;
+				index++;
+			}
+			using->next=(token*) size;
+			using=using->next;
+			using->type=3;
+			name[e+1]='\0';
+			strcpy(using->value,name);
+		}else{
+			printf("Pas compris : %c ( place dans utf : %i)\n",c,c );
+			index++;
+		}
+	}
+	return tokens;
 }
 
 int main(int argc, char const *argv[]) {
-  token* t=tokenize(exemple,sizeof(exemple)/sizeof(exemple[0]));
-  while(t->next){
-    printf("%s\n",t->value);
-    t=t->next;
-  }
-  return 0;
+	printf("%i\n",sizeof(exemple)/sizeof(exemple[0]) );
+	token * t = tokenize(exemple,sizeof(exemple)/sizeof(exemple[0]));
+	while(t->next){
+		printf("Type : %i Valeur : %s\n",t->type, t->value);
+		t=t->next;
+	}
+	return 0;
 }
