3ecf6d883c7fe4d1b64bce5927cde0e2a80b8332
momoladebrouill <momoladebrouill@gmail.com>
2022-04-11 17:22:43 +0200

    tokenizer fonctionne au poil :thumb:

--- cµ/tokenizer.c
+++ cµ/tokenizer.c
@@ -9,7 +9,7 @@
 3: identifier
 */
 
-const char exemple[]="<µ> <loq> || Hello world ! || 125 </loq>  </µ> \0";
+const char exemple[]="<µ><a></a> 12 ||allah est grand||   58 <a> ||squid game||</µ>";
 
 int isletter(char s){return ('a'<=s && s<='z') || ('A'<=s && s<='Z');}
 int isnumber(char s){return ('0'<=s && s<='9') || s=='.' || s=='-';}
@@ -26,56 +26,33 @@ token * tokenize(const char text[],int len){
 	token * using = tokens -> next;
 	int index=0;
 	char c;
-	while(index < len ){
+	while(index < len-1 ){
 		c=text[index];
 		if(c==' ' || c=='\n' || c=='\t'){
 			index++;
-		}else if(c=='|' && index+1<len){
-			int lenOfStr=0;
-			if(text[index+1]=='|'){
-				index++;
-				while(text[index]!='|' && text[index+1]!= '|'){
-					index++;
-					lenOfStr++;
-				}
-			}
-			using->next=(token*)size;
-			using=using->next;
-			using->type=0;
-			char p[lenOfStr+1];
-			for(int i=0;i<lenOfStr;i++){
-				p[i]=text[index-lenOfStr+i];
-			}
-			p[lenOfStr+1]='\0';
-			strcpy(using->value,p);
-		}else if(c=='<'){
+        }else if(c=='<'){
 			char name[100];
-			index++;
 			int e=0;
+            index++;
 			while(index<len && text[index] !='>'){
-				name[e]=text[index];
-				e++;
-				index++;
+				name[e++]=text[index++];
 			}
-			index++;
+            index++;
 			using->next=(token *) size;
 			using=using->next;
 			using->type=1;
-			name[e+1]='\0';
+			name[e]='\0';
 			strcpy(using->value,name);
-		}else if(isnumber(c)){
+        }else if(isnumber(c)){
 			char name[100];
-			index++;
 			int e=0;
 			while(index<len && isnumber(text[index])){
-				name[e]=text[index];
-				e++;
-				index++;
+				name[e++]=text[index++];
 			}
 			using->next=(token *) size;
 			using=using->next;
 			using->type=2;
-			name[e+1]='\0';
+			name[e]='\0';
 			strcpy(using->value,name);
 		}else if(isletter(c)){
 			char name[100];
@@ -91,18 +68,30 @@ token * tokenize(const char text[],int len){
 			using->type=3;
 			name[e+1]='\0';
 			strcpy(using->value,name);
+        }else if(c=='|'){
+			int lenOfStr=0;
+            char name[100];
+            index+=2;
+			while(!(text[index]=='|' && text[index+1]== '|')){
+				name[lenOfStr++]=text[index++];
+			}
+            index+=2;
+			using->next=(token*)size;
+			using=using->next;
+			using->type=0;
+			name[lenOfStr]='\0';
+			strcpy(using->value,name);
 		}else{
 			printf("Pas compris : %c ( place dans utf : %i)\n",c,c );
 			index++;
 		}
 	}
-	return tokens;
+	return tokens->next->next;
 }
 
 int main(int argc, char const *argv[]) {
-	printf("%i\n",sizeof(exemple)/sizeof(exemple[0]) );
 	token * t = tokenize(exemple,sizeof(exemple)/sizeof(exemple[0]));
-	while(t->next){
+	while(t){
 		printf("Type : %i Valeur : %s\n",t->type, t->value);
 		t=t->next;
 	}
